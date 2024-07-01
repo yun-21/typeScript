a981a632e856f35a4689d7ad3890b98ddc033a27
shinjiyun <ranji12@naver.com>
2024-07-01 10:50:05 +0900

    js에서 integer조건 주기

---
+++ work-1.js
@@ -0,0 +1,11 @@
+function a(b){
+  if(typeof(b)==="number"){
+    if(Number.isInteger(b)===true){
+      let result = b +10;
+      return result
+    }
+  }
+}
+
+console.log(a("신지윤"))
+console.log("1"+1)
