755fe94a60f5a278f288ebd272994719f4c66478
shinjiyun <ranji12@naver.com>
2024-07-08 10:15:34 +0900

    typescript코드 컴파일 성공

---
+++ dist/enum.js
@@ -0,0 +1,9 @@
+"use strict";
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let color = Color.Green;
+//# sourceMappingURL=enum.js.map
